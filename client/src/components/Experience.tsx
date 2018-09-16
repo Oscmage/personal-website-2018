decb0a5fc19d8fca9a1babfcf5f8bfdd0d8555eb
Oscar Evertsson <oscar.evertsson@live.com>
2018-09-16 20:27:16 +0200

    Add fetch from backend and fix date display.

--- client/src/components/Experience.tsx
+++ client/src/components/Experience.tsx
@@ -15,33 +15,46 @@ interface ITimeLineData {
   title: string;
 }
 
-class Experience extends React.Component {
+class Experience extends React.Component<
+  {},
+  { experienceList: ITimeLineData[] }
+> {
+  public constructor(props: any) {
+    super(props);
+    this.state = {
+      experienceList: []
+    };
+  }
+
+  public componentDidMount() {
+    this.getExperienceList()
+      .then((res: ITimeLineData[]) => this.setState({ experienceList: res }))
+      .catch((err: Error) => console.log(err));
+  }
+
+  public getExperienceList = async (): Promise<ITimeLineData[]> => {
+    const response = await fetch("/api/experience");
+    const body = await response
+      .json()
+      .then(data => data as { experienceList: ITimeLineData[] });
+
+    if (response.status !== 200) {
+      throw Error("Fail");
+    }
+    return body.experienceList;
+  };
+
   public render() {
-    const list: ITimeLineData[] = [
-      {
-        description:
-          "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
-        endDate: new Date("2018-08-20"),
-        present: true,
-        startDate: new Date("2017-07-20"),
-        subTitle: "Miami, FL",
-        title: "Creative Director"
-      },
-      {
-        description:
-          "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
-        endDate: new Date("2018-08-20"),
-        present: true,
-        startDate: new Date("2017-07-20"),
-        subTitle: "Miami, FL",
-        title: "Creative Director"
-      }
-    ];
-    const verticalTimelineElement = list.map(elem => (
+    const experienceList = this.state.experienceList;
+    const verticalTimelineElement = experienceList.map(elem => (
       <VerticalTimelineElement
         key={elem.title}
         className="vertical-timeline-element--work"
-        date={this.getDateAsString(elem.startDate, elem.endDate, elem.present)}
+        date={this.getWorkingDurationString(
+          elem.startDate,
+          elem.endDate,
+          elem.present
+        )}
         iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
       >
         <h3 className="vertical-timeline-element-title">{elem.title}</h3>
@@ -49,6 +62,16 @@ class Experience extends React.Component {
         <p>{elem.description}</p>
       </VerticalTimelineElement>
     ));
+
+    if (experienceList === []) {
+      return (
+        <div className="ExperienceContainer">
+          <h2>Experience</h2>
+          <p>Error fetching experience list</p>
+        </div>
+      );
+    }
+
     return (
       <div className="ExperienceContainer">
         <h2>Experience</h2>
@@ -57,37 +80,62 @@ class Experience extends React.Component {
     );
   }
 
-  private getDateAsString(
+  private getWorkingDurationString(
     startDate: Date,
     endDate: Date,
     present: boolean
   ): string {
+    // Javascript is stupid
+    if (typeof endDate === "string") {
+      endDate = new Date(endDate);
+    }
+    if (typeof startDate === "string") {
+      startDate = new Date(startDate);
+    }
+
     const diff = new Date(endDate.getTime() - startDate.getTime());
     const years = diff.getUTCFullYear() - 1970;
-    const yString = years > 1 ? " years, " : " year, ";
-    const months = diff.getUTCMonth();
-    const mString = months > 1 ? " months)" : " month)";
+
+    const months = diff.getMonth();
     if (present) {
       return (
-        startDate.getFullYear() +
-        " - Present" +
-        " (" +
-        years +
-        yString +
-        months +
-        mString
+        this.getDateAsString(startDate) +
+        " - Present " +
+        this.getDurationString(years, months)
       );
     }
     return (
-      startDate.getFullYear() +
+      this.getDateAsString(startDate) +
       " - " +
-      endDate.getFullYear() +
-      " (" +
-      years +
-      yString +
-      months +
-      mString
+      this.getDateAsString(endDate) +
+      " " +
+      this.getDurationString(years, months)
     );
+  }
+
+  private getDateAsString(date: Date): string {
+    const year = date.getFullYear();
+    const m = date.getMonth();
+    const month = m < 10 ? "0" + m : m + "";
+    const d = date.getDay();
+    const day = d < 10 ? "0" + d : d + "";
+    return year + "/" + month + "/" + day;
+  }
+
+  private getDurationString(years: number, months: number): string {
+    let yString = "(";
+    if (years === 1) {
+      yString = "(1 year, ";
+    } else if (years > 1) {
+      yString = "(" + years + " years, ";
+    }
+
+    if (months === 0) {
+      return yString + ")";
+    } else if (months === 1) {
+      return yString + "1 month";
+    }
+    return yString + months + " months)";
   }
 }
 
--- server.js
+++ server.js
@@ -3,8 +3,28 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/api/hello", (req, res) => {
-  res.send({ express: "Hello From Express" });
+app.get("/api/experience", (req, res) => {
+  res.send({
+    experienceList: [
+      {
+        description: "Own firm working primarily with web",
+        endDate: new Date(),
+        present: true,
+        startDate: new Date("2017-06-01"),
+        subTitle: "Gothenburg",
+        title: "Own Firm"
+      },
+      {
+        description:
+          "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+        endDate: new Date("2018-08-31"),
+        present: false,
+        startDate: new Date("2018-06-12"),
+        subTitle: "Stockholm",
+        title: "Backend Developer Intern - Spotify"
+      }
+    ]
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
